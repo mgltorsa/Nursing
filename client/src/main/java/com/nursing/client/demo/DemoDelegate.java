1b15f272c277a86e4059d1c0269c2c0957614233
mgltorsa <mgltorsa@gmail.com>
2019-05-18 16:08:16 -0500

    updated properties

--- client/src/main/java/com/nursing/client/demo/DemoDelegate.java
+++ client/src/main/java/com/nursing/client/demo/DemoDelegate.java
@@ -13,24 +13,19 @@ public class DemoDelegate {
 	@Value("${host_basepath}")
 	private String hostBasepath;
 	
-	@Value("${host_port}")
-	private int port;
-	
 	private RestTemplate restTemplate = new RestTemplate();
-	
 	public void getCar(long id) {
 		
 		String url = getUrlService();
 		System.out.println("url was-> "+url);
 		// ResponseEntity<Car> entity = restTemplate.getForEntity(url+"/cars/1", Car.class);
 		// Car car = entity.getBody();
-		
 		// return car;
 	}
 
 	private String getUrlService() {
 //		return "http://"+host+":"+port+hostBasepath;
-		return "http://localhost:8080/api";
+		return "https://nursing-rest.herokuapp.com/api";
 	}
 	
 }
