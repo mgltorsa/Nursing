89db9084e759a3f4f60ec48dbc02eef5b97ddeae
Hagia <mauricio.hernandez2@correo.icesi.edu.co>
2019-05-21 12:07:00 -0500

    GET methods improved

--- rest/src/main/java/com/nursing/rest/controller/MedicineController.java
+++ rest/src/main/java/com/nursing/rest/controller/MedicineController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import javax.websocket.server.PathParam;
 
+import org.hibernate.validator.constraints.pl.REGON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nursing.rest.dao.IMedicineDao;
@@ -25,23 +27,23 @@ public class MedicineController {
 	}
 	
 	
-	@GetMapping(value = "/medicines/{name}")
-	public List<Medicine> getMedicinesByName(@PathVariable String name){
+	@GetMapping(value = "/medicines", params = "name")
+	public List<Medicine> getMedicinesByName(@RequestParam String name){
 		return medicineService.findByName(name);
 	}
 	
-	@GetMapping(value = "/medicines/gn-{genericName}")
-	public List<Medicine> getMedicinesByGenericName(@PathVariable String genericName){
+	@GetMapping(value = "/medicines", params = "genericName")
+	public List<Medicine> getMedicinesByGenericName(@RequestParam String genericName){
 		return medicineService.findByGenericName(genericName);
 	}
 	
-	@GetMapping("/medicines/lb-{lab}")
-	public List<Medicine> getMedicineByLab(@PathVariable String lab){
+	@GetMapping(value = "/medicines", params = "lab")
+	public List<Medicine> getMedicineByLab(@RequestParam String lab){
 		return medicineService.findByLaboratory(lab);
 	}
 	
-	@GetMapping("/medicines/at-{administrationType}")
-	public List<Medicine> getMedicinesByAdministrationType(@PathVariable String administrationType){
+	@GetMapping(value = "/medicines", params = "administrationType")
+	public List<Medicine> getMedicinesByAdministrationType(@RequestParam String administrationType){
 		return medicineService.findByAdministrationType(administrationType);
 	}
 }
