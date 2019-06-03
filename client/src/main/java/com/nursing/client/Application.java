126fc3dc8b3770d1da636320cf2fed4360a33b83
mgltorsa <mgltorsa@gmail.com>
2019-06-02 23:35:13 -0500

    added own tests

--- client/src/main/java/com/nursing/client/Application.java
+++ client/src/main/java/com/nursing/client/Application.java
@@ -1,6 +1,7 @@
 package com.nursing.client;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.client.RestTemplate;
 
+import com.nursing.client.delegate.Delegate;
+import com.nursing.client.model.Medicine;
+import com.nursing.client.model.Patient;
+
 
 
 @SpringBootApplication
@@ -16,9 +21,19 @@ import org.springframework.web.client.RestTemplate;
 public class Application {
 	
 
+	@Autowired
+	private Delegate delegate;
 
 	public static void main(String[] args) {
-		 SpringApplication.run(Application.class, args);
+		 Application app = SpringApplication.run(Application.class, args).getBean(Application.class);
+//		 Patient patient = new Patient("1107527450","Miguel","Torres");
+//		 Patient pa = app.delegate.save(patient, Patient.class);
+//		 System.out.println(pa);
+//		 Medicine medicine = app.delegate.get(1l, Medicine.class);
+//		 System.out.println(medicine);
+//		 Patient pe = app.delegate.get("1107527450",Patient.class);
+//		 System.out.println(pe);
+		 
 		
 	}
 	
